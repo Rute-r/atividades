c6ee453993f5bbef4758958f5c882d44bc455a27
Rute-r <rute.s.r.sousa@gmail.com>
2024-10-28 14:42:04 -0300

    commit da atividade 1

--- src/atividades/Atividade1.java
+++ src/atividades/Atividade1.java
@@ -1,9 +1,28 @@
 package atividades;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Atividade1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner ler = new Scanner(System.in);
+		ler.useLocale(Locale.US); //Mudei a localidade para aceitar input com ponto ao inves de virgula como solicita na atividade
+        
+        float salario, abono, novoSalario;
+        
+        System.out.println("Digite o Salário: ");
+        salario = ler.nextFloat();
+        
+        System.out.println("Digite o Abono: ");
+        abono = ler.nextFloat();
+        
+        novoSalario = salario + abono;
+        
+        System.out.println("Novo Salário: " + novoSalario);
+        
+        ler.close();
 
 	}
 
