dbe84c37da56af511b5daa5cc3473c8508b211f4
Rute-r <rute.s.r.sousa@gmail.com>
2024-10-30 14:08:56 -0300

    Preparando o ambiente e commit de atividade1

---
+++ src/loops/Atividade1.java
@@ -0,0 +1,32 @@
+package loops;
+
+import java.util.Scanner;
+
+public class Atividade1 {
+
+	public static void main(String[] args) {
+		//pegar valor de intervalo
+		Scanner scanner = new Scanner(System.in);
+		int primeiroNum, segundoNum;
+		
+		System.out.println("Digite o primeiro número do intervalo: ");
+		primeiroNum = scanner.nextInt();
+		
+		System.out.println("Digite o segundo número do intervalo: ");
+		segundoNum = scanner.nextInt();
+		
+		if (primeiroNum > segundoNum) {
+			System.out.println("Intervalo inválido!");
+		} else {
+			for(int i = primeiroNum; i < segundoNum; i++) {
+				if(i % 5 == 0 && i % 3 == 0) {
+					System.out.println(i + " é multiplo de 3 e 5");
+				}
+			}
+		}
+		
+		scanner.close();
+
+	}
+
+}
