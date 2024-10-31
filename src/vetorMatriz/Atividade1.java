772a9b89714f0b0fac7a007551f752d1cf7a767a
Rute-r <rute.s.r.sousa@gmail.com>
2024-10-31 14:57:40 -0300

    commit da atividade 1

--- src/vetorMatriz/Atividade1.java
+++ src/vetorMatriz/Atividade1.java
@@ -1,9 +1,32 @@
 package vetorMatriz;
 
+import java.util.Scanner;
+
 public class Atividade1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int vetorNumeros[] = { 2, 5, 1, 3, 4, 9, 7, 8, 10, 6 };
+		int numero, posicao = 0;
+		boolean vazia = true;
+
+		System.out.println("Digite o número que você deseja encontrar");
+		numero = scanner.nextInt();
+
+		for (int i = 0; i < vetorNumeros.length; i++) {
+			if (numero == vetorNumeros[i]) {
+				posicao = i;
+				vazia = false;
+			}
+		}
+
+		if (!vazia) {
+			System.out.println("O número " + numero + " está localizado na posição: " + posicao);
+		} else {
+			System.out.println("O número " + numero + " não foi encontrado!");
+		}
+		
+		scanner.close();
 
 	}
 
