4abf3f72296e9d141b1d923f9adf34a62d1c5f0d
Rute-r <rute.s.r.sousa@gmail.com>
2024-11-01 13:30:29 -0300

    commit da atividade 1

--- src/collection/Atividade1.java
+++ src/collection/Atividade1.java
@@ -1,10 +1,35 @@
 package collection;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Atividade1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<String> cores = new ArrayList<String>();
+		String cor;
 
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Digite a " + (i + 1) + "° cor: ");
+			cor = scanner.next();
+			cores.add(cor);
+		}
+
+		System.out.println("\nListar todas as cores: \n");
+		for (String corLista : cores) {
+			System.out.println(corLista);
+		}
+
+		Collections.sort(cores);
+
+		System.out.println("\nOrdenar as cores: \n");
+		for (String corOrdenada : cores) {
+			System.out.println(corOrdenada);
+		}
+
+		scanner.close();
 	}
 
 }
